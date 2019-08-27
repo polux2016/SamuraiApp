445fb42baec9ff0fd4240de9764454058813ec6d
Андрей Полюхович <poluhovichandrey@gmail.com>
2019-08-27 18:12:52 +0300

    Reinint SQLite DB

---
+++ SamuraiApp.Migrations.SqlLite/Migrations/20190827150747_RemoveReqiredColumnsAndMore.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Migrations.SqlLite.Migrations
+{
+    public partial class RemoveReqiredColumnsAndMore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
